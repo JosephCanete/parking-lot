60f66472cd77742b886cd47903e666b0ade520d6
Raymond Joseph Cañete <raymondcanete1997@gmail.com>
2022-07-12 12:44:55 +0800

    Started App

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./styles/App.scss";
+import Home from "./components/Home";
+import Container from "react-bootstrap/Container";
+
+export default function App() {
+  return <h1> whaaat?</h1>;
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import Navigation from "./Navigation";
+import Header from "./Header";
+import Cards from "./Cards";
+
+export default function Home() {
+  return (
+    <>
+      <Navigation />
+      <Home />
+    </>
+  );
+}
